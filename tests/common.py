d421021d70e23595b6b82418665c2b90fcba255f
Yuriy Chebyshev <ychebyshev@it.ru>
2022-02-05 01:03:44 +0300

    add config for tests

--- tests/common.py
+++ tests/common.py
@@ -8,9 +8,8 @@ app_dir = Path(__file__).parent.parent
 def create_alembic_config() -> Config:
     # Create Alembic configuration object
     # (we don't need database for getting revisions list)
-    config_name = app_dir / "alembic.ini"
+    config_name = app_dir / "tests" / "config" / "alembic.ini"
     config = Config(file_=str(config_name))
-    config.set_main_option("sqlalchemy.url", "postgresql+asyncpg://postgres:postgres@localhost:5432/daily_test")
     config.set_main_option("script_location", str(app_dir / "migrations"))
     return config
 
