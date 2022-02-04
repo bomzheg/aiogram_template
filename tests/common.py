050128547364051978db0d12dfee2b00a8886c12
Yuriy Chebyshev <ychebyshev@it.ru>
2022-02-05 01:05:28 +0300

    refactoring alembic in tests

--- tests/common.py
+++ tests/common.py
@@ -10,6 +10,5 @@ def create_alembic_config() -> Config:
     # (we don't need database for getting revisions list)
     config_name = app_dir / "tests" / "config" / "alembic.ini"
     config = Config(file_=str(config_name))
-    config.set_main_option("script_location", str(app_dir / "migrations"))
     return config
 
