5c9f277c3df997d02805f2010abf017f68535713
Yuriy Chebyshev <ychebyshev@it.ru>
2022-02-12 17:58:37 +0300

    logging now settings up from main only

--- app/__main__.py
+++ app/__main__.py
@@ -5,16 +5,20 @@ from aiogram import Dispatcher, Bot
 from aiogram.dispatcher.filters import ContentTypesFilter
 
 from app.config import load_config
+from app.config.logging_config import setup_logging
 from app.handlers import setup_handlers
 from app.middlewares import setup_middlewares
+from app.models.config.main import Paths
 from app.models.db import create_pool
 
 logger = logging.getLogger(__name__)
 
 
 def main():
-    app_dir = Path(__file__).parent.parent
-    config = load_config(app_dir)
+    paths = Paths(Path(__file__).parent.parent)  # TODO get this dir from env
+
+    setup_logging(paths)
+    config = load_config(paths)
 
     dp = Dispatcher()
     dp.message.bind_filter(ContentTypesFilter)
--- app/config/main.py
+++ app/config/main.py
@@ -1,22 +1,15 @@
 import logging.config
-from pathlib import Path
 
 import yaml
-from dotenv import load_dotenv
 
 from app.config.db import load_db_config
-from app.config.logging_config import setup_logging
 from app.models.config import Config
 from app.models.config.main import Paths, BotConfig
 
 logger = logging.getLogger(__name__)
 
 
-def load_config(app_dir: Path) -> Config:
-    paths = Paths(app_dir)
-
-    load_dotenv(paths.app_dir / ".env")
-    setup_logging(paths)
+def load_config(paths: Paths) -> Config:
     with (paths.config_path / "config.yaml").open("r") as f:
         config_dct = yaml.safe_load(f)
     return Config(
