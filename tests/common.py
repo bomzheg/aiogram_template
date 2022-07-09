d16752587e8cdfb6230c29624218187921d939ee
Yuriy Chebyshev <ychebyshev@it.ru>
2022-07-09 17:43:57 +0300

    add tests

--- tests/common.py
+++ tests/common.py
@@ -1,3 +1,4 @@
+import logging
 from pathlib import Path
 
 from alembic.config import Config as AlembicConfig
@@ -24,3 +25,18 @@ def create_app_config() -> Config:
     paths = Paths(tests_dir)
     setup_logging(paths)
     return load_config(paths)
+
+
+def create_test_container():
+    import sqlalchemy
+    from testcontainers.mysql import MySqlContainer
+
+    with MySqlContainer('mysql:5.7.17') as mysql:
+        engine = sqlalchemy.create_engine(mysql.get_connection_url())
+        version, = engine.execute("select version()").fetchone()
+        print(version)
+
+
+if __name__ == '__main__':
+    logging.basicConfig(level=logging.DEBUG)
+    create_test_container()
