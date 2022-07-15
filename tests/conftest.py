5bf7a398460e92fccd06fc7cd5d82d0543b08091
Yuriy Chebyshev <bomzheg@gmail.com>
2022-07-15 18:04:49 +0300

    fix test

--- tests/conftest.py
+++ tests/conftest.py
@@ -21,4 +21,9 @@ def paths():
 
 @pytest.fixture(scope="session")
 def event_loop():
-    return asyncio.get_event_loop()
+    try:
+        return asyncio.get_running_loop()
+    except RuntimeError:
+        loop = asyncio.new_event_loop()
+        asyncio.set_event_loop(loop)
+        return loop
