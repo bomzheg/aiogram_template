4c5ffd2ff523a0e0b8ee56d78144a7bfe57b46e6
Yuriy Chebyshev <bomzheg@gmail.com>
2022-07-31 14:28:43 +0300

    add few more tests, refactoring exists, add workaround for windows integration tests (until testcontainers does not work)

--- tests/integration/conftest.py
+++ tests/integration/conftest.py
@@ -1,18 +1,27 @@
 import logging
+import os
 
 import pytest
+import pytest_asyncio
 from sqlalchemy.ext.asyncio import AsyncSession, create_async_engine
 from sqlalchemy.orm import sessionmaker
 from testcontainers.postgres import PostgresContainer
 
+from app.dao.holder import HolderDao
+from app.models.config import Config
 
 logger = logging.getLogger(__name__)
 
 
-@pytest.fixture
+@pytest_asyncio.fixture
 async def session(pool: sessionmaker) -> AsyncSession:
-    async with pool() as session:
-        yield session
+    async with pool() as session_:
+        yield session_
+
+
+@pytest_asyncio.fixture
+async def dao(session: AsyncSession) -> HolderDao:
+    return HolderDao(session=session)
 
 
 @pytest.fixture(scope="session")
@@ -24,9 +33,11 @@ def pool(postgres_url: str) -> sessionmaker:
 
 
 @pytest.fixture(scope="session")
-def postgres_url() -> str:
-    with PostgresContainer("postgres:11") as postgres:
-        postgres_url = postgres.get_connection_url().replace("psycopg2", "asyncpg")
-        logger.info("postgres url %s", postgres_url)
-        yield postgres_url
-
+def postgres_url(app_config: Config) -> str:
+    if os.name == "nt":  # windows testcontainers now not working
+        yield app_config.db.uri
+    else:
+        with PostgresContainer("postgres:11") as postgres:
+            postgres_url_ = postgres.get_connection_url().replace("psycopg2", "asyncpg")
+            logger.info("postgres url %s", postgres_url_)
+            yield postgres_url_
